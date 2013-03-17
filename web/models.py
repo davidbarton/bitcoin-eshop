488f765e888e5da735ae8ac533c644130abff250
David Barton <david@dreamorreal.com>
2013-03-17 16:10:41 +0100

    Indetation

--- web/models.py
+++ web/models.py
@@ -5,13 +5,13 @@ class Order(models.Model):
 	product = models.CharField(max_length=30)
 	count = models.IntegerField()
 	customer = models.CharField(max_length=60)
-    email = models.CharField(max_length=80)
-    customer_address = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    transaction_accepted = models.BooleanField(default=False)
+	email = models.CharField(max_length=80)
+	customer_address = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	transaction_accepted = models.BooleanField(default=False)
 
-    class Meta:
-        ordering = ['-created']
+	class Meta:
+		ordering = ['-created']
 
-    def __unicode__(self):
-        return u'%s' % self.wallet_address
+	def __unicode__(self):
+		return u'%s' % self.wallet_address
