55c683c13f5b114f50dab807494caaee8d4838fe
David Barton <david@dreamorreal.com>
2013-03-17 17:22:18 +0100

    Added index view

--- web/views.py
+++ web/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from web.models import Order
+
+def index(request):
+    # now return the rendered template
+    return render(request, 'web/index.html', {})
