4ec8f06340f8ebcee801622479a8aae3b201dbdb
David Barton <david@dreamorreal.com>
2013-03-17 16:07:20 +0100

    Indetation

--- web/models.py
+++ web/models.py
@@ -1,5 +1,5 @@
 from django.db import models
- 
+
 class Order(models.Model):
 	wallet_address = models.CharField(max_length=34)
 	product = models.CharField(max_length=30)
@@ -9,9 +9,9 @@ class Order(models.Model):
     customer_address = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     transaction_accepted = models.BooleanField(default=False)
- 
+
     class Meta:
         ordering = ['-created']
- 
+
     def __unicode__(self):
         return u'%s' % self.wallet_address
