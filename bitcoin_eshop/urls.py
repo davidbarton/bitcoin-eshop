b3655fe0b3b8b8d9aebf86a715b091537dffd8df
David Barton <david@dreamorreal.com>
2013-04-13 04:18:20 +0200

    Static Urls update

--- bitcoin_eshop/urls.py
+++ bitcoin_eshop/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -20,3 +21,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+   urlpatterns += staticfiles_urlpatterns()
